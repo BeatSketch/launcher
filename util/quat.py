4e213acc3bed192537f724db5f8282059aacec08
Janis Hutz <development@janishutz.com>
2026-04-25 15:26:18 +0200

    chore: Prepare for supporting the dtypes in python

---
+++ util/quat.py
@@ -0,0 +1,4 @@
+# NOTE: Scipy has native(ish) support for quaternions,
+# so depends on what you want to use. Other alternatives:
+# https://github.com/KieranWynn/pyquaternion (https://pypi.org/project/pyquaternion/)
+# https://pypi.org/project/numpy-quaternion/
