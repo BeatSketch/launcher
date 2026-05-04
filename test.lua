31c00f98ecd74fb8611817ca3b2934bb32d83ee9
Janis Hutz <development@janishutz.com>
2026-05-04 16:43:31 +0200

    chore: Remove testing files

--- test.lua
+++
@@ -1,7 +0,0 @@
-for _, value in ipairs(arg) do
-    print(value)
-end
-
-for line in io.lines() do
-    print(line)
-end
--- test.py
+++
@@ -1,7 +0,0 @@
-from util.ipc.decode import BeatSketchInstanceDataDecoder
-
-
-com = BeatSketchInstanceDataDecoder()
-
-while True:
-    print(com.get_data())
