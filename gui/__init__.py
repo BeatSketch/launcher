35967edc436a76bd53e0cbbc23cf99e021180a64
Janis Hutz <development@janishutz.com>
2026-05-06 15:37:55 +0200

    chore: Move packaging to packaging repo

--- gui/__init__.py
+++ gui/__init__.py
@@ -6,10 +6,9 @@ import PyQt6.QtCore as qtcore
 from gui.config import create_config_interface, ident_func
 
 
-# TODO: we should probably close it when the VR app launches
-# We can then re-launch the GUI when the VR app exits (except user hits exit to desktop in VR?)
 def create_launcher_app(launch_func: Callable[[], None] = ident_func):
-    # TODO: Theming, maybe using this: https://pypi.org/project/qt-themes/
+    # TODO: Theming? (not really needed because QT Themes will make it feel more native)
+    # If so, maybe using this: https://pypi.org/project/qt-themes/
     app = qt.QApplication([])
     window = qt.QMainWindow()
     window.setWindowTitle("BeatSketch Launcher")
@@ -17,7 +16,6 @@ def create_launcher_app(launch_func: Callable[[], None] = ident_func):
     box = qt.QVBoxLayout()
 
     title_wrapper = qt.QHBoxLayout()
-    # NOTE: Pyright may show this as error. It isn't.
     title_wrapper.setAlignment(qtcore.Qt.AlignmentFlag.AlignCenter)
     t = qt.QLabel()
     t.setText("BeatSketch Launcher")
