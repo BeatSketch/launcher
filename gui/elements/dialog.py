64530abd8d25e0e7d75b4e5890d12336356cb8c6
Janis Hutz <development@janishutz.com>
2026-05-07 11:58:55 +0200

    feat(UI): Dialog warning of missing gamescope for launch

---
+++ gui/elements/dialog.py
@@ -0,0 +1,75 @@
+from typing import Callable, TypedDict
+from PyQt6.QtGui import QFont
+from PyQt6.QtWidgets import (
+    QDialog,
+    QHBoxLayout,
+    QLabel,
+    QMessageBox,
+    QPushButton,
+    QVBoxLayout,
+)
+
+
+class DialogAction(TypedDict):
+    name: str
+    callback: Callable[[], None]
+    close_dialog: bool
+
+
+default_action: DialogAction = {
+    "name": "Ok",
+    "callback": lambda: None,
+    "close_dialog": True,
+}
+
+
+def open_flexible_dialog(
+    title: str, actions: list[DialogAction] = [default_action], msg: str = ""
+):
+    dialog = QDialog()
+    dialog.setWindowTitle("BeatSketch")
+
+    layout = QVBoxLayout()
+
+    title_widget = QLabel()
+    title_widget.setFont(QFont("sans", 30))
+    title_widget.setText(title)
+    layout.addWidget(title_widget)
+
+    if msg != "":
+        msg_widget = QLabel()
+        msg_widget.setText(msg)
+        layout.addWidget(msg_widget)
+
+    if len(actions) > 0:
+        actions_layout = QHBoxLayout()
+        for action in actions:
+
+            def cb():
+                dialog.close()
+                action["callback"]()
+
+            button = QPushButton()
+            button.setText(action["name"])
+            button.pressed.connect(cb)
+            actions_layout.addWidget(button)
+        layout.addLayout(actions_layout)
+
+    dialog.setLayout(layout)
+    dialog.exec()
+
+
+def open_msg_dialog(
+    msg: str,
+    actions: QMessageBox.StandardButton = QMessageBox.StandardButton.Ok,
+    title: str = ""
+):
+    dialog = QMessageBox()
+    if title == "":
+        dialog.setWindowTitle("BeatSketch")
+    else:
+        dialog.setWindowTitle(title)
+    dialog.setText(msg)
+    dialog.setStandardButtons(actions)
+
+    dialog.exec()
--- util/subprocess_manager.py
+++ util/subprocess_manager.py
@@ -1,8 +1,25 @@
+import colorama
 import multiprocessing as mp
+import os
+import platform
+import subprocess as sp
 from util.ipc import BeatSketchVRApplication
+from gui.elements import dialog
 
 global proc
 proc: None | mp.Process = None
+
+
+def _wayland_checks() -> bool:
+    if platform.system() == "Linux":
+        # Detect Wayland
+        if (
+            os.getenv("XDG_BACKEND") == "wayland"
+            or os.getenv("XDG_SESSION_TYPE") == "wayland"
+        ):
+            status = sp.run(["which", "gamescope"], capture_output=True, text=True).stdout
+            return status != ""
+    return False
 
 
 def start_vr_app(args: list[str]):
@@ -10,8 +27,17 @@ def start_vr_app(args: list[str]):
     if proc and proc.is_alive():
         return False
 
+    if not _wayland_checks():
+        dialog.open_msg_dialog(
+            "Gamescope is not installed. Please install it using your distribution's package manager",
+            title="Launching VR Application failed",
+        )
+        return False
+
+    print(colorama.Fore.GREEN + colorama.Style.DIM + "==> Pre-Launch checks passed, launching VR app" + colorama.Style.RESET_ALL)
     proc = mp.Process(target=_run, args=(args,))
     proc.start()
+
     return True
 
 
