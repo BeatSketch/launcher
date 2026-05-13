ccd7c91c421a393faf33e6d1974e04ab872bfaaa
Janis Hutz <development@janishutz.com>
2026-05-13 16:42:39 +0200

    feat(dataset): filesystem walker

---
+++ dataset/tree.py
@@ -0,0 +1,14 @@
+import os
+
+
+def filesystem_walker(dir: str) -> list[str]:
+    files = os.listdir(dir)
+    file_list = []
+    for file in files:
+        path = dir + "/" + file
+        if os.path.isdir(path):
+            file_list += filesystem_walker(path)
+        else:
+            file_list.append(path)
+
+    return file_list
