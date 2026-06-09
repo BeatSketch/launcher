0ad4af8af5ef1fd546b53bddf4e277b724e0ba62
Janis Hutz <development@janishutz.com>
2026-06-09 11:40:11 +0200

    feat: improved config manager

---
+++ util/config/__init__.py
@@ -0,0 +1,70 @@
+from typing import Literal, cast as _cast
+
+from util.config import save as _save
+from util.config.dtype import BeatSketchConfig
+from util.config.load import (
+    load_config_file as _load_config_file,
+    default_config as _default_config,
+    validate as _validate,
+)
+from util.config.merge import merge as _merge
+
+
+def load_and_validate_config(path: str) -> tuple[bool, BeatSketchConfig]:
+    """Load the configuration file from the specified path and validate it with the jsonschema
+
+    Args:
+        path: The path to the config file to load
+
+    Returns:
+        A tuple containing validation status and a valid config (if validation is False, the default config)
+    """
+    try:
+        loaded_config = _load_config_file(path)
+    except Exception:
+        return (False, _default_config())
+
+    global _config
+    global _config_location
+
+    _config_location = path
+
+    if _validate(loaded_config):
+        conf = _cast(
+            BeatSketchConfig, _merge(_cast(dict, _default_config()), loaded_config)
+        )
+        _config = conf
+        return (True, conf)
+    else:
+        _config = _default_config()
+        return (False, _default_config())
+
+
+global _config
+_config = _default_config()
+
+global _config_location
+_config_location = "config.yml"
+
+
+def get_config() -> BeatSketchConfig:
+    return _config
+
+
+def update_rotation(side: Literal["x", "y", "z"], value: str):
+    try:
+        validated = int(value)
+
+        global _config
+        _config["saber_angle"][side] = validated
+    except Exception:
+        pass
+
+
+def update_enabled_cleanup(kind: Literal["distance", "collisions"], enabled: bool):
+    global _config
+    _config["cleanup"][kind] = enabled
+
+
+def save_config():
+    _save.save_config(_config, _config_location)
---
+++ util/config/load.py
@@ -0,0 +1,61 @@
+import jsonschema as _jsonschema
+import json as _json
+import yaml as _yaml
+
+from util.config.dtype import BeatSketchConfig
+
+# Load schema
+with open("config.schema.json") as file:
+    _schema = _json.load(file)
+    file.close()
+
+
+def load_config_file(file: str) -> dict:
+    """Loads and parses the yaml file at the specified location
+
+    Args:
+        file: Filepath to the config file to be loaded
+
+    Returns:
+        A dict containing the parsed yaml file
+    """
+    with open(file, "r") as f:
+        parsed = _yaml.load(f, Loader=_yaml.FullLoader)
+    return parsed
+
+
+def validate(config: dict | list) -> bool:
+    """Validates the provided config
+
+    Args:
+        config: The config to validate
+
+    Returns:
+        True if the config passed schema validation, false otherwise
+    """
+    try:
+        _jsonschema.validate(config, _schema)
+    except _jsonschema.SchemaError:
+        print("Schema invalid")
+        return False
+    except _jsonschema.ValidationError:
+        print("Config invalid")
+        return False
+
+    return True
+
+
+def default_config() -> BeatSketchConfig:
+    """Get the default configuration. Is used as a starting position for the config
+
+    Returns:
+        The default configuration
+    """
+    return {
+        "saber_angle": {"x": -20, "y": 0, "z": 0},
+        "default_save_path": "~/BeatSketch/",
+        "folder_loc_for_picker": "~/Downloads/",
+        "mirror": False,
+        "used_model": "mlp",
+        "cleanup": {"collisions": True, "distance": True},
+    }
