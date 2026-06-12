d1b63f0a889c50cd3c922d121e098e8d5d488043
Janis Hutz <development@janishutz.com>
2026-06-12 13:30:34 +0200

    fix: improve vibration config

--- map_handler/__init__.py
+++ map_handler/__init__.py
@@ -33,7 +33,7 @@ def launch_wrapper(
         f"ry={conf['saber_angle']['y']}",
         f"rz={conf['saber_angle']['z']}",
         f"njs={njs}",
-        f"vibrate={conf['vibrate']}",
+        f"vibrate={'true' if conf['vibrate'] else 'false'}",
     ]
     if testing_mode:
         args = ["dev=true"]
